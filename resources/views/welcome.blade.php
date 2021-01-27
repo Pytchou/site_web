f2bc16375a40fe78ae5d883d36db4286d70e81cd
thomasBarille-ab <thomasBarille-ab@github.com>
2021-01-27 14:58:38 +0100

    début header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Banque alimentaire</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="resources/css/app.css" rel="stylesheet">
         <link href="resources/css/responsive.css" rel="stylesheet">
     </head>
 
-    <body class="antialiased">
+    <body>
         <header class="xLarge-12 large-12 medium-12 small-12 xSmall-12">
-            <div>
-                
+            <div class="xLarge-6 large-6 medium-12 small-12 xSmall-12" id=#head1>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Nobis fuga vitae suscipit neque. Molestiae exercitationem, ratione quia, 
+                ad amet nam laboriosam minus aut voluptatem illo laudantium sequi temporibus,
+                 mollitia culpa!
+            </div>
+            <div class="xLarge-6 large-6 medium-12 small-12 xSmall-12"id=#head2>
+                <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.quimperle.bzh%2Fwp-content%2Fuploads%2F2020%2F11%2Fcollecte_alimentaire_2020-1200x630.jpg&imgrefurl=https%3A%2F%2Fwww.quimperle.bzh%2Factualite%2Fcollecte-banque-alimentaire%2F&tbnid=-M3JbPjHGNfQsM&vet=12ahUKEwiospz8n7zuAhXOw4UKHcu3DlAQMygAegUIARCjAQ..i&docid=2w5kyqyFVg1ETM&w=1200&h=630&q=banque%20alimentaire&ved=2ahUKEwiospz8n7zuAhXOw4UKHcu3DlAQMygAegUIARCjAQ" alt="">
             </div>
         </header>
     </body>

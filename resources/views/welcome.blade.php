4f667747ba5245fff4afd3d183402a49cfcd0223
Paul-Rchd <prouchaud@hotmail.fr>
2021-01-28 06:18:15 +0100

    modifs stylax un max

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,57 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
     <title>Site web</title>
     <link href="css/app.css" rel="stylesheet"></head>
 <body>
 <header>
 
 </header>
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 24.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-<svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     viewBox="0 0 141.73 5.67" style="enable-background:new 0 0 141.73 5.67;" xml:space="preserve">
-<style type="text/css">
-    .st0{fill:#0F388E;}
-</style>
-    <polygon class="st0" points="141.73,4.07 141.73,5.67 0,5.67 0,0 "/>
-</svg>
+
+<main>
+
+    <!-- <img src="{{asset('media/images/footer-form.svg')}}"> -->
+
+    <section class="column wrap">
+        <div class="xLarge-12 large-12 medium-12 small-12 xSmall-12 column">
+            <h1 class="padd-around center colab"> Certains de nos partenaires</h1>
+            <div id="logo-container">
+                <div class="xLarge-3 large-3 medium-3 small-6 xSmall-6 border-logos shadow-logos center column containers">
+                    <h2 style="padding: 15px;">Croix rouge</h2>
+                    <hr>
+                    <img id="croix-rouge" src="{{asset('media/images/croix-rouge.png')}}" >                    
+                </div>
+                <div class="xLarge-3 large-3 medium-3 small-6 xSmall-6 border-logos shadow-logos center column containers">
+                    <h2 style="padding: 15px;">Banque alimentaire</h2>
+                    <hr>
+                    <img id="banque-alimentaire" src="{{asset('media/images/logo_banque_alimentaire.png')}}" >
+                </div>
+                <div class="xLarge-3 large-3 medium-3 small-6 xSmall-6 border-logos shadow-logos center column containers">
+                    <h2 style="padding: 15px;">Resto du coeur</h2>
+                    <hr>
+                    <img id="resto-du-coeur" src="{{asset('media/images/Restos_du_coeur_Logo.png')}}" >               
+                </div>
+                <div class="xLarge-3 large-3 medium-3 small-6 xSmall-6 border-logos shadow-logos center column containers">
+                    <h2 style="padding: 15px;">Secours populaire</h2>
+                    <hr>
+                    <img id="secours-populaire" src="{{asset('media/images/Secours_populaire_logo.png')}}" >        
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <xml version="1.0" encoding="utf-8"?>
+    <!-- Generator: Adobe Illustrator 24.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+    <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 141.73 5.67" style="enable-background:new 0 0 141.73 5.67;" xml:space="preserve">
+    <style type="text/css">
+        .st0{fill:#0F388E;}
+    </style>
+        <polygon class="st0" points="141.73,4.07 141.73,5.67 0,5.67 0,0 "/>
+    </svg>
+</main>
+    
 <footer>
     <footer>
         <section class="row wrap" id="foot">
@@ -24,13 +60,13 @@
                 <div class="padd-around">
                     <p>Informations</p>
                     <ul>
-                        <li>
+                        <li class="center">
                             <a href="/rgpd">RGPD</a>
                         </li>
-                        <li>
+                        <li class="center">
                             <a href="/cgu">CGU</a>
                         </li>
-                        <li>
+                        <li class="center">
                             <a href="/mentions-legales">Mentions Légales</a>
                         </li>
                     </ul>
@@ -42,10 +78,10 @@
                         Nos différents partenaires
                     </p>
                     <ul>
-                        <li><a href="https://www.banquealimentaire.org/" target="_blank">Les banques alimentaires</a></li>
-                        <li><a href="https://www.restosducoeur.org//" target="_blank">Les restos du coeur</a></li>
-                        <li><a href="https://www.secourspopulaire.fr/" target="_blank">Le secours populaire</a></li>
-                        <li><a href="https://www.croix-rouge.fr/" target="_blank">La croix rouge</a></li>
+                        <li class="center"><a href="https://www.banquealimentaire.org/" target="_blank">Les banques alimentaires</a></li>
+                        <li class="center"><a href="https://www.restosducoeur.org//" target="_blank">Les restos du coeur</a></li>
+                        <li class="center"><a href="https://www.secourspopulaire.fr/" target="_blank">Le secours populaire</a></li>
+                        <li class="center"><a href="https://www.croix-rouge.fr/" target="_blank">La croix rouge</a></li>
                     </ul>
                 </div>
             </div>
@@ -53,10 +89,10 @@
                 <div class="padd-around">
                     <p>Contact</p>
                     <ul>
-                        <li>
+                        <li class="center">
                             <a href="mailto:email@example.com">email@exemple.com</a>
                         </li>
-                        <li>
+                        <li class="center">
                             <a href="tel:+33781815136">0781815136</a>
                         </li>
                     </ul>

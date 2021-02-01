0523b89eb63b05f6e65f53ddd7c7e4e585ff9fc1
Paul-Rchd <prouchaud@hotmail.fr>
2021-02-01 18:24:32 +0100

    Moi je suis différent et du coup je n'écris pas comme les autres

---
+++ database/migrations/2021_02_01_165039_partners.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Partners extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigIncrements('volunteers_max_score');
+            $table->string('siret')->nullable();
+            $table->string('naf')->nullable();
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('contact');
+            $table->string('address');
+            $table->string('address_details');
+            $table->string('zip');
+            $table->string('city');
+            $table->decimal('longitude')->nullable();
+            $table->decimal('latitude')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}

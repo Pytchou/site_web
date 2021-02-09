f25d203e93331f5f005764314050e5a801bd9133
ThibaultBarral <thibault.barral3347@gmail.com>
2021-02-09 16:32:23 +0100

    dashboard text

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -66,13 +66,13 @@
 
         <div id="bloc" class="xLarge-12 large-12 medium-12 small-12 xSmall-12 center">
             <div class="xLarge-3 large-3 medium-12 small-12 xSmall-12">
-                <h1>Rejoindre une campagne</h1>
-                <p>De nombreuses campagnes d'aide alimentaires sont disponibles via nos
-                differents partenaires. Cliquez sur le bouton ci-dessous pour en rejoindre une.</p>
+                <h1>Ajouter un Evénement</h1>
+                <p>Create a new project on ProManage. Collaborate
+                    your work. Directory to your local projects</p>
                 <div style="align-items: center;display: flex;">
                     <a href="#" class="btn2">+</a>
-                    <p class="padd-around" style="text-transform: uppercase; color: #0F388E; font-weight: bold; font-size: 18px">
-                        Rejoindre une campagne
+                    <p class="padd-around" style="text-transform: uppercase; color: #EC4E6E; font-weight: bold; font-size: 18px">
+                        Create New Project
                     </p>
                 </div>
             </div>
@@ -177,7 +177,7 @@
                 </div>
             </div>
         </div>
-
+        
     </section>
 
 </main>

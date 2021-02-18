65ca520c10fd31b766604927e233e0eb90899af2
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-02-18 21:06:10 +0100

    Envoi des emails

--- resources/views/emails/registered_partner.blade.php
+++ resources/views/emails/registered_partner.blade.php
@@ -4,7 +4,7 @@
 <p>L'association {{$request->name_partner}} vient de se créer un compte sur le site web mais il doit être confirmé.</p>
 <p>Merci de cliquer sur le lien suivant et de procéder à la validation des informations.</p>
 
-@component('mail::button', ['url' => "/partenaire/confirm/{$request->name_partner}/{$confirmation_token}"])
+@component('mail::button', ['url' => url("/partenaire/confirm/{$request->name_partner}/" . urlencode($confirmation_token))])
 Confirmer mon compte
 @endcomponent
 

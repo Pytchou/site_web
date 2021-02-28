64297f5ba68aade63fe12bc82995127bf2e13214
Theodsg <71518113+Theodsg@users.noreply.github.com>
2021-02-28 15:30:44 +0100

    Create team_desktop.blade.php

---
+++ resources/views/auth/partner/team_desktop.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="../css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+
+<header>
+    <section class="row wrap">
+        <nav>
+            <div class="logo-nav">
+                <img src="{{asset('media/images/croix-rouge.png')}}" alt="">
+            </div>
+            <ul>
+                <li>
+                    <a href="">Tableau de bord</a>
+                </li>
+                <li>
+                    <a href="">Toutes les campagnes</a>
+                </li>
+                <li>
+                    <a href="">Membres</a>
+                </li>
+                <li>
+                    <a href="">Statistiques</a>
+                </li>
+                <li>
+                    <a href="">Assistance</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+</header>
+<section class="row wrap">
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc1">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc2">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc3">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc1">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc2">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="xLarge-4 large-4 medium-12 small-12 xSmall-12">
+        <div class="padd-around">
+            <div class="padd-around column" id="bloc3">
+                <h3 class="center">Nom Prénom</h3>
+                <p class="center">
+                    Description
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
